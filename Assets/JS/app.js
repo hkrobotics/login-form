399d4569611a79f15d9808cdb0ac85fe3a5823e6
hkrobotics <kumarhemant24jan@gmail.com>
2021-11-20 19:37:47 +0530

    add aleart on sucessfull submission 🚀

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -5,14 +5,21 @@ function validateform() {
 
     if (name == null || name == "") {
         alert("🙅‍♀️ Name can't be blank ");
+        console.warn("🙅‍♀️ Name can't be blank ");
         return false;
     } else if (username.length < 6) {
         alert("🙅‍♂️ Username must be at least 6 characters long");
+        console.warn("🙅‍♂️ Username must be at least 6 characters long");
         return false;
     }
     else if (password.length < 6) {
         alert("🙅 Password must be at least 6 characters long");
+        console.warn("🙅 Password must be at least 6 characters long");
         return false;
     }
+    else {  
+        alert("✅ Registration Sucessfull");
+        console.alert("✅ Registration Sucessfull");  
+     }
 
 }
